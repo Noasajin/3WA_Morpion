f45f1862af8f5fe06109481eabc82e3876a34b12
Noasajin <theo.facorat@gmail.com>
2022-12-19 16:06:47 +0000

    Code fonctionnel

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
+///// MORPION
+let morpion = ["", "", "", "", "", "", "", "", ""]
+
+
 ///// TABLEAU DES POSSIBILITES
-const morpion = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+const victory = [
+    [1, 2, 3],
+    [4, 5, 6], [7, 8, 9],
                 [1, 4, 7], [2, 5, 8], [3, 6, 9],
                 [1, 5, 9], [3, 5, 7]]
-                
 
-///// CONDITIONS DE VICTOIRE
-function victory() {
-    morpion.forEach(possibility => {
-        if (possibility[0] !== "" && possibility[1] !== "" && possibility[2] !== "") {
-            if (possibility[0] === possibility[1] && possibility[0] === possibility[2]) {
-            alert('Vous avez gagné');
-            }     
+
+let turn = document.getElementById('turn');
+    turn = 0
+    // turn.innerHTML = 'Tours :';
+
+
+function isWinner() {
+    victory.forEach(casDeVictoire => {
+        const indexNb1 = casDeVictoire[0]
+        const indexNb2 = casDeVictoire[1]
+        const indexNb3 = casDeVictoire[2]
+
+        console.log(morpion[indexNb1],morpion[indexNb2],morpion[indexNb3])
+        if (morpion[indexNb1] !== "" && morpion[indexNb2] !== "" && morpion[indexNb3] !== "") {
+            if (morpion[indexNb1] === morpion[indexNb2] && morpion[indexNb2] === morpion[indexNb3]) {
+                alert('Le gagnant est : ' + morpion[indexNb1]);
+            }
         }
+
     })
-}
+    }
+
 
 document.addEventListener('DOMContentLoaded', () => {
     let caseOne = document.getElementById('1');
@@ -27,21 +44,20 @@ document.addEventListener('DOMContentLoaded', () => {
     let caseNine = document.getElementById('9');
     const cases = [caseOne, caseTwo, caseThree, caseFour, caseFive, caseSix, caseSeven, caseEight, caseNine]
     
-    let turn = document.getElementById('turn');
-    turn = 0
-    // turn.innerHTML = 'Tours :';
-    
     
     ///// INTERAGIR AVEC 'X' et 'O'
     const allCases = (e) => {
+        const index = e.getAttribute("id")
         if (turn === 0) {
+            morpion[index] = '╳';
             e.innerHTML = `<p>╳</p>`;
             turn++
         } else if (turn >= 1) {
+            morpion[index] = '◯';
             e.innerHTML = `<p>◯</p>`;
             turn--
         }
-        victory();
+        isWinner();
     }
     
     cases.forEach(function(item){
