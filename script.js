ceadd89f04fdaf04bc0737c9ae5652ff7b6de23c
Noasajin <theo.facorat@gmail.com>
2022-12-19 09:41:09 +0000

    Init

--- script.js
+++ script.js
@@ -1,67 +1,20 @@
-const quizz = [{
-        question: 'Pourquoi Dorian est aussi beau ?',
-        reponse: ['Il a fait une chirurgie', 'C\'est un don', 'Il a eu de la chance'],
-        goodReponse: 'Il a eu de la chance'
-    },
-    {
-        question: 'Pourquoi Isaac est aussi intelligent ?',
-        reponse: ['Il est né comme ça', 'On lui a transféré du QI', 'Il s\'est cogné fort contre un mur en étant bébé'],
-        goodReponse: 'Il est né comme ça'
-    },
-    {
-        question: 'Pourquoi Théo est aussi drôle ?',
-        reponse: ['Il est né comme ça', 'On lui a transféré de l\'humour', 'Il a mangé un clown'],
-        goodReponse: 'Il a mangé un clown'
-    }
-]
+const morpion = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                [1, 5, 9], [3, 5, 7]]
 
-let idQuestion = 0
-let score = 0
-
-
-///// FUNCTION CHOOSE QUESTION
-function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-
-///// VARIABLE ALEATOIRE QUESTIONS
-let isQuestion = randomIntFromInterval(1, 3);
+let caseOne = document.getElementById('1');
+let caseTwo = document.getElementById('2');
+let caseThree = document.getElementById('3');
+let caseFour = document.getElementById('4');
+let caseFive = document.getElementById('5');
+let caseSix = document.getElementById('6');
+let caseSeven = document.getElementById('7');
+let caseEight = document.getElementById('8');
+let caseNine = document.getElementById('9');
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const question = document.getElementsByTagName('p');
-    const reponseA = document.getElementById('reponseA');
-    const reponseB = document.getElementById('reponseB');
-    const reponseC = document.getElementById('reponseC');
-    let goodReponse = null;
-
-
-    ///// FUNCTIONS QUESTION - ANSWER
-    function showQuestion() {
-        question[0].innerText = quizz[idQuestion].question;
-        reponseA.innerText = quizz[idQuestion].reponse[0];
-        reponseB.innerText = quizz[idQuestion].reponse[1];
-        reponseC.innerText = quizz[idQuestion].reponse[2];
-        goodReponse = quizz[idQuestion].goodReponse
-    }
-    
-    function onClick(btn) {
-        if (btn.innerText === goodReponse) {
-            alert('Bien joué !!');
-            score++
-        } else {
-            alert('T\'es trop nul');
-            score--
-        }   
-        
-        idQuestion++
-        showQuestion()
-    }
-    
-    reponseA.addEventListener('click', () => onClick(reponseA));
-    reponseB.addEventListener('click', () => onClick(reponseB));
-    reponseC.addEventListener('click', () => onClick(reponseC));
-    
-    showQuestion()
+    caseOne.addEventListener('click', () => {
+        caseOne.innerHTML('<p class="answerX">X</p>');
+    })
 });
